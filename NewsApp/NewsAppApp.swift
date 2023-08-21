ee090110ac5990f432c095e91a6fc55287fc9721
VishwanathChenni <vishwanathchenni@gmail.com>
2023-08-21 11:37:03 +0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Vishwanath Chenni on 21/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
